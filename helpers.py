c603a6903be4695588e22b3f0847c640c5b55d78
Robert <haha@nope.lol>
2020-12-15 19:45:07 +0000

    initial python

---
+++ helpers.py
@@ -0,0 +1,31 @@
+###
+# helpers.py
+#
+# Helper functions for use in this project
+#
+# Usage: from helpers import *
+###
+
+from shutil import which
+import subprocess
+import yaml
+import os.path
+
+configPath = "config.yml"
+
+def getConfig():
+    if !os.path.isfile(configPath):
+        print("%s does not exist. Copy example.config.yml to this path and edit with your configuration" % (configPath))
+        return False
+    with open(configPath) as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+def sh(command):
+    subprocess.call(command.split(' '))
+
+def grab(command):
+    return str(subprocess.check_output(command.split(' ')), 'utf-8')
+
+def isCommand(name):
+    """Check whether `name` is on PATH and marked as executable."""
+    return which(name) is not None
---
+++ snapshot.py
@@ -0,0 +1,50 @@
+###
+# snapshot.py
+#
+# Generate a timestamped snapshot of the Lightning Netowrk graph
+# Must be run by a node with lnd installed
+# Optional: copy the graph.json snapshot over ssh 
+#
+# Usage: Run script with cron
+#
+#   python3 snapshot.py 
+#   python3 snapshot.py --remote-write
+###
+
+import os
+import time
+import json
+from helpers import isCommand, sh, grab, getConfig 
+
+config = getConfig()
+
+def getSnapshot():
+    """Get LND to describe the network graph"""
+    data = {}
+    data["timestamp"] = int(time.time)
+    data["graph"] = json.loads(grab("lncli describegraph"))
+    return data
+
+def remoteWrite(string):
+    open(config["tempfile"], "w").write(string)
+    sh('scp %s %s:%s' % (config["tempfile"], config["remote_host"], config["remote_file"]))
+    os.remove(config["tempfile"])
+
+def localWrite(string):
+    open(config["localfile"], "w").write(string)
+
+def main():
+    if config is False:
+        print("Config file error. Exiting.")
+        return 
+    if !isCommand("lncli"):
+        print("LND is not installed. Exiting.")
+        return
+    snapshot = json.dumps(getSnapshot())
+    if "--remote-write" in sys.argv:
+        remoteWrite(snapshot)
+    else:
+        localWrite(snapshot)
+
+if __name__ == '__main__':
+    main()
